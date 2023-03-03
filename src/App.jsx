9d607b4fee0a756cab0f3912b78d319e09d2465f
covi-ensil <ensil@covi.co.kr>
2023-03-03 18:45:43 +0900

    REFACTORING App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import { FcSearch } from 'react-icons/fc';
-import Menus from './jjongjjongFavoriteMenus.json';
 import { useState } from 'react';
 import logo from './images/considering.gif';
-import video from './images/snow.mp4'
-import food from './logo.svg';
+import video from './images/snow.mp4';
 import Clock from './components/Clock';
+import Menus from './jjongjjongFavoriteMenus.json';
 
 function App() {
   const [menu, setMenu] = useState('몰?루');
 
-  // 0 < Math.random() <1
-  // 여기서 데이터의 길이만큼 곱한 후 parseInt로 정수화시키면 해당 데이터의 길이를 넘지않는 랜덤한 수를 구할 수 있음!
-  const getRandomIndex = (length) => {
-    return parseInt(Math.random() * length);
-  };
+  // Refactor getRandomIndex to a one-liner function
+  const getRandomIndex = (length) => parseInt(Math.random() * length);
 
   const btnStyle = {
     color: 'white',
@@ -26,30 +22,29 @@ function App() {
     lineHeight: 1.5,
   };
 
-  const RandomMenu = () => {
-    let RandomINdex = getRandomIndex(Object.keys(Menus).length);
-    let MenuList = Object.keys(Menus);
-    setMenu((menu) => MenuList[RandomINdex]);
+  const handleRandomMenu = () => {
+    const randomIndex = getRandomIndex(Object.keys(Menus).length);
+    const menuList = Object.keys(Menus);
+    setMenu(menuList[randomIndex]);
   };
 
   return (
-    <div className=' w-full h-screen bg-white'>
-              <div className=' m'>
-          <Clock />
-        </div>
+    <div className='w-full h-screen bg-white'>
+      <div className='m'>
+        <Clock />
+      </div>
       <div className='flex flex-col justify-center items-center w-full h-full gap-4'>
         <h1 className='text-xl md:text-3xl'>Dr. JjongJjong's Menu Big Data</h1>
         <div className='px-12'>
-          <img className=' rounded-2xl' src={logo} alt='considering...'></img>
+          <img className='rounded-2xl' src={logo} alt='considering...'></img>
         </div>
-        {/* <span className='text-8xl'>🤔</span> */}
-        <span className=' text-blue-500'>
+        <span className='text-blue-500'>
           오늘의 저녁 메뉴는?
-          <button onClick={() => RandomMenu()} style={btnStyle}>
+          <button onClick={handleRandomMenu} style={btnStyle}>
             <FcSearch />
-          </button>{' '}
+          </button>
         </span>
-        <div>{' ✨ ' + menu + ' ✨'}</div>
+        <div>{`✨ ${menu} ✨`}</div>
       </div>
     </div>
   );
