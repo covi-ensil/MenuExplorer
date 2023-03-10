c468b476d54682219d789840acaf9a0b57006c4f
covi-ensil <ensil@covi.co.kr>
2023-03-10 15:55:40 +0900

    UPDATE Build files

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { FcSearch } from 'react-icons/fc';
 import { useState } from 'react';
 import logo from './images/considering.gif';
 import Clock from './components/Clock';
-import Menus from './datas/jjongjjongFavoriteMenus.json';
-import top69Menus from './datas/jjongjjongFavoriteMenus.json';
+import jjongjjongMenus from './datas/jjongjjongFavoriteMenus.json';
+import top69Menus from './datas/top69Menus.json';
 import MenuOption from './components/MenuOption';
 
 function App() {
-    const [menu, setMenu] = useState('몰?루');
     const [selectedMode, setSelectedMode] = useState('선택된 모드가 없습니다');
+    const [menu, setMenu] = useState('몰?루');
+    const [price, setPrice] = useState('가격')
+    const [popularity, setPopularity] = useState('인기도')
 
     function handleModeSelect(event) {
+        setMenu('몰?루')
         setSelectedMode(event.target.value);
     }
 
@@ -29,9 +32,28 @@ function App() {
     };
 
     const handleRandomMenu = () => {
-        const randomIndex = getRandomIndex(Object.keys(Menus).length);
-        const menuList = Object.keys(Menus);
-        setMenu(menuList[randomIndex]);
+        let randomIndex, menuList;
+        if (selectedMode === '🕊️') {
+            randomIndex = getRandomIndex(Object.keys(jjongjjongMenus).length);
+            menuList = Object.keys(jjongjjongMenus);
+            console.log('list :', menuList)
+            setMenu(menuList[randomIndex]);
+        }
+        if (selectedMode === '🍱') {
+            // console.log('selectedMode :', selectedMode)
+            randomIndex = getRandomIndex(Object.keys(top69Menus).length);
+            // console.log(randomIndex)
+            menuList = Object.keys(top69Menus);
+            console.log('list :', menuList)
+            setMenu(menuList[randomIndex]);
+
+            const priceList = Object.values(top69Menus).map(el => el.가격)
+            const poplarityList = Object.values(top69Menus).map(el => el.인기도)
+            // console.log(priceList)
+            setPrice(priceList[randomIndex])
+            setPopularity(poplarityList[randomIndex])
+        }
+
     };
 
     return (
@@ -42,7 +64,7 @@ function App() {
             <div className='mx-auto text-right pr-12 text-sm'>
                 <p className='py-1'> 🕹️ : {selectedMode}</p>
             </div>
-            <div className='flex flex-col justify-center items-center w-full pt-12'>
+            <div className='flex flex-col justify-center items-center w-full py-12'>
                 <MenuOption
                     selectedMenu={selectedMode}
                     handleModeSelect={handleModeSelect}
@@ -54,30 +76,42 @@ function App() {
                 </h1>
                 <div className='px-12'>
                     <img
-                        className='rounded-2xl'
+                        className='rounded-2xl w-96 max-w-lg'
                         src={logo}
                         alt='considering...'
                     ></img>
                 </div>
                 {selectedMode === '선택된 모드가 없습니다' ? (
-                                        <div>
-                                        <span className='text-blue-500'>
-                                            선택된 모드가 없습니다
-                                            <button style={btnStyle}>
-                                                <FcSearch />
-                                            </button>
-                                        </span>
-                                        <div>어떤 메뉴를 먹어야 잘 먹었다고 소문이 날까</div>
-                                    </div>
-                ) : (
-                    <div>
-                        <span className='text-blue-500'>
-                            오늘의 저녁 메뉴는?
-                            <button onClick={handleRandomMenu} style={btnStyle}>
-                                <FcSearch />
+                    <div className='flex flex-col'>
+                        <span className='text-center text-blue-500'>
+                            선택된 모드가 없습니다
+                            <button style={btnStyle}>
+                                <FcSearch className='w-6 h-6' />
                             </button>
                         </span>
-                        <div>{`✨ ${menu} ✨`}</div>
+                        <div> 🥺 모드를 선택해 주세요 🥺 </div>
+                    </div>
+                ) : selectedMode === '🕊️' ? (
+                    <div className='flex flex-col w-full'>
+                        <span className='text-center text-blue-500'>
+                            오늘의 쫑이 메뉴는?
+                            <button onClick={handleRandomMenu} style={btnStyle}>
+                                <FcSearch className='w-6 h-6' />
+                            </button>
+                        </span>
+                        <div className='text-center'>{`✨ ${menu} ✨`}</div>
+                    </div>
+                ) : (
+                    <div className='flex flex-col w-full'>
+                        <span className='text-center text-blue-500'>
+                            오늘의 저녁 인기 메뉴는?
+                            <button onClick={handleRandomMenu} style={btnStyle}>
+                                <FcSearch className='w-6 h-6' />
+                            </button>
+                        </span>
+                        <div className='text-center'>{`✨ ${menu} ✨`}</div>
+                        {/* <div className='text-center'>{`✨ 가격 : ${price} ✨`}</div>
+                        <div className='text-center'>{`✨ 인기도 : ${popularity} ✨`}</div> */}
                     </div>
                 )}
             </div>
