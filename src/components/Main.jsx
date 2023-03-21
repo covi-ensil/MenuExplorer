147f1c5665c1d68a9d5f246da5d1f96318636541
covi-ensil <ensil@covi.co.kr>
2023-03-21 12:47:28 +0900

    ADD ad area

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -63,7 +63,7 @@ const Main = ({ selectedMode, selectedMenu, menuImg, handleRandomMenu, isDarkMod
                     </span>
 
                     <div className='content'>
-                        <div className={`${isDarkMode? 'text-white' : 'text-black'} text-center  py-4`}>{`✨ ${selectedMenu} ✨`}</div>
+                        <div className={`${isDarkMode? 'text-white' : 'text-black'} text-center  py-2`}>{`✨ ${selectedMenu} ✨`}</div>
                         {menuImg === defaultImg ? (
                             <img
                                 className='mx-auto w-56 h-60 max-w-xs rounded-lg'
