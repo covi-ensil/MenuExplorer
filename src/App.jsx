ae14bf0f9d9f3511cd7f886bb3b3dc6e30f5159a
covi-ensil <ensil@covi.co.kr>
2023-03-22 18:36:22 +0900

    BUILD css

--- src/App.jsx
+++ src/App.jsx
@@ -140,7 +140,7 @@ function App() {
                             🕹️ : {selectedMode}
                         </p>
                     </div>
-                    <div className='flex flex-col justify-center items-center w-full pb-10'>
+                    <div className='flex flex-col justify-center items-center w-full pt-4 pb-10'>
                         <MenuOption handleModeSelect={handleModeSelect} isDarkMode={isDarkMode} />
                     </div>
                     <div className='flex flex-col justify-center items-center w-full h-[60%] gap-4 max-w-screen-lg xl:max-w-screen-2xl'>
