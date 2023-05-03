e909c05de9c37ad921363cb9af93cbd3ba4ba6b6
covi-ensil <ensil@covi.co.kr>
2023-05-03 14:59:58 +0900

    BUILD latest player version

--- src/components/Coviplayer.jsx
+++ src/components/Coviplayer.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 const Coviplayer = () => {
+
+    const [count, setCount] = useState(0)
+    const countRef = useRef(count)
+
+    useEffect(() => {
+        countRef.current = count
+    }, [count])
+
+    useEffect(() => {
+        return () => {
+            console.log("unmount 시 출력", countRef.current)
+        }
+    }, [])
+
     return (
         <>
             <div className='covi 코비'></div>
             <div className='test 코비'></div>
+            <button className='clicker' onClick={() => setCount(c => c + 1)}>+</button>
+            <div>hello</div>
         </>
     );
 };
